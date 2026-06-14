9b0aa22dddfaa66481a6a27d3e8c6171dc2b845a
lajohnnysj-create <la.johnny.sj@gmail.com>
2026-06-14 16:02:56 -0700

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Bee Yappers",
+  title: "Bleviq",
   description: "An AI chat widget trained on your website.",
 };
 
