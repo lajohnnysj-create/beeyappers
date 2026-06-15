af3461563ef7342280bb2b5aef8eb20be8f23499
lajohnnysj-create <la.johnny.sj@gmail.com>
2026-06-14 23:23:39 -0700

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,15 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Bleviq",
   description: "An AI chat widget trained on your website.",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
+      { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
